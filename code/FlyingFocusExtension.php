de076d86df6bb8fdd13f9cd4007a0fabcfb31ede
Jonathan Alpers <jonathan.alpers@dia.govt.nz>
2019-03-20 14:24:09 +1300

    exposes javascript to public folder

--- code/FlyingFocusExtension.php
+++ code/FlyingFocusExtension.php
@@ -6,7 +6,7 @@ use SilverStripe\View\Requirements;
 class FlyingFocusExtension extends Extension {
 
 	public function onAfterInit() {
-		Requirements::javascript(MODULE_FLYINGFOCUS_DIR.'/javascript/flying-focus.js');
+		Requirements::javascript('resources/vendor/dia-nz/silverstripe-flying-focus/javascript/flying-focus.js');
 	}
 
 }
