bf21359eeeca5066f46262b1e7f987049c6f795d
Jonathan Alpers <jonathan.alpers@dia.govt.nz>
2019-03-20 13:49:46 +1300

    adds namespace lookups

--- code/FlyingFocusExtension.php
+++ code/FlyingFocusExtension.php
@@ -1,10 +1,12 @@
 <?php
-	
+
+use SilverStripe\Core\Extension;
+use SilverStripe\View\Requirements;
+
 class FlyingFocusExtension extends Extension {
-	
+
 	public function onAfterInit() {
 		Requirements::javascript(MODULE_FLYINGFOCUS_DIR.'/javascript/flying-focus.js');
-	} 
-	
-}
+	}
 
+}
